9e9e92721ab49b9e94a167473d2f0d7f3303b87a
Ezequiel <delaluzezequiel@gmail.com>
2020-09-25 10:39:17 -0500

    Final commit

--- docs/js/contador.js
+++ docs/js/contador.js
@@ -3,21 +3,27 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Sep 26, 2020 00:00:00').getTime(),
+let countDown = new Date('Sep 25, 2020 10:28:00').getTime(),
     x = setInterval(function() {    
 
       let now = new Date().getTime(),
           distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
       //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+      if (distance <= 0) {
+        document.getElementById('days').innerText = 0,
+        document.getElementById('hours').innerText = 0,
+        document.getElementById('minutes').innerText = 0,
+        document.getElementById('seconds').innerText = 0;
+       clearInterval(x);
+       setTimeout(()=>{ window.location.href = "birthday.html";},2000);
+      //  window.location.href = "birthday.html";
+      }
 
     }, second)
+
--- js/contador.js
+++ js/contador.js
@@ -9,15 +9,21 @@ let countDown = new Date('Sep 26, 2020 00:00:00').getTime(),
       let now = new Date().getTime(),
           distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
       //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+      if (distance <= 0) {
+        document.getElementById('days').innerText = 0,
+        document.getElementById('hours').innerText = 0,
+        document.getElementById('minutes').innerText = 0,
+        document.getElementById('seconds').innerText = 0;
+       clearInterval(x);
+       setTimeout(()=>{ window.location.href = "birthday.html";},2000);
+      //  window.location.href = "birthday.html";
+      }
 
     }, second)
+
